6bc80a9a2a14fc1deaaad27aea4b3a9c803d846b
Matija Rifl <matija.rifl@gmail.com>
2018-02-24 09:02:16 +0100

    commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import jinja2
 import webapp2
+import random
 
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
@@ -26,11 +27,46 @@ class BaseHandler(webapp2.RequestHandler):
         template = jinja_env.get_template(view_filename)
         return self.response.out.write(template.render(params))
 
+class GlavnoMesto:
+    def __init__(self, gl_mesto, drzava, slika):
+        self.gl_mesto = gl_mesto
+        self.drzava = drzava
+        self.slika = slika
+
+def vnos_podatkov():
+    lj = GlavnoMesto("Ljubljana", "Slovenija", "/assets/slike/ljubljana.jpg")
+    bu = GlavnoMesto("Budimpesta", "Madzarska", "/assets/slike/budimpesta.jpg")
+    du = GlavnoMesto("Dunaj", "Avstrija", "/assets/slike/dunaj.jpeg")
+    be = GlavnoMesto("Berlin", "Nemcija", "/assets/slike/berlin.jpg")
+    pa = GlavnoMesto("Pariz", "Francija", "/assets/slike/pariz.jpg")
+    ri = GlavnoMesto("Rim", "Italija", "/assets/slike/rim.jpg")
+
+    return [lj, bu, du, be, pa, ri]
+
 
 class MainHandler(BaseHandler):
     def get(self):
-        return self.render_template("hello.html")
+        gl_mestece = vnos_podatkov()[random.randint(0,5)]
+        info = {"mestece": gl_mestece}
+        return self.render_template("zacetna_stran.html",info)
+
+class UgibajHandler(BaseHandler):
+    def post(self):
+        ugibano = self.request.get("mesto")
+        preveri_drzavo = self.request.get("preveri")
+
+        glavna_mesta = vnos_podatkov()
+        for item in glavna_mesta:
+            if item.drzava == preveri_drzavo:
+                if item.gl_mesto.lower() == ugibano.lower():
+                    result = True
+                else:
+                    result = False
+                info = {"rezultat": result, "enota": item}
+
+                return self.render_template("ugibalna_stran.html", info)
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
+    webapp2.Route('/ugibaj', UgibajHandler),
 ], debug=True)
